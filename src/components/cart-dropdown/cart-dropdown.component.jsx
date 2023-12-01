6339274fc4dc4c9c19e0f509533c666b05e11263
jed peek <jedpeek@MacBook-Pro-3.local>
2023-12-01 09:24:10 -0800

    added items to cartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,16 @@
 import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
-
+import CartItem from "../cart-item/cart-item.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 function CartDropdown() {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items" />
+      {cartItems.map((item) => (
+        <CartItem cartItem={item} key={item.id} />
+      ))}
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
