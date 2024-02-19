82d106a87103797d29a923142137fb539acadb9c
jed peek <jedpeek@MacBook-Pro-3.local>
2024-02-19 09:03:21 -0800

    removed class warnings

--- src/routes/success/success.jsx
+++ src/routes/success/success.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import { selectCartItems } from "../../store/cart/cart.selector";
 import { useSelector } from "react-redux";
 import { downloadFromFirebase } from "../../utils/firebase/firebase.utils";
-import getStripe from "../../utils/stripe/stripe.utils";
-// get stripe session from stripe API
+
 export const Success = () => {
   const cartItems = useSelector(selectCartItems);
-  const queryParams = new URLSearchParams(window.location.search);
-  const value = queryParams.get("session_id");
-  console.log(value);
-  useEffect(() => {
-    const getLineItems = async () => {
-      const stripe = await getStripe();
-      const lineItems = await stripe.checkout;
-      console.log("LINE ITEMS: ", lineItems);
-    };
-    getLineItems();
-  }, []);
+
   return (
     <div className="container">
       <div className="flex items-center align-center">
         <h1 className="text-5xl">THANK YOU FOR YOUR PURCHASE</h1>
       </div>
 
-      <div class="container px-5 py-12 mx-auto">
-        <div class="flex flex-wrap -m-4">
+      <div className="container px-5 py-12 mx-auto">
+        <div className="flex flex-wrap -m-4">
           {cartItems.map((cartItem) => {
             return (
               <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
